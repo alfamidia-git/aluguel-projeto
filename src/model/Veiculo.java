645e8bf1ef8a2f56dfc47676cb0ca66acc38318e
alfamidia-git <marlon@alfamidia.com.br>
2022-08-29 20:24:41 -0300

    Criado todos os modelos de classe

--- src/model/Veiculo.java
+++ src/model/Veiculo.java
@@ -1,5 +1,46 @@
 package model;
 
-public class Veiculo {
+import util.Contador;
 
+public class Veiculo {
+	
+	private Integer id;
+	
+	private String modelo;	
+	private String marca;
+	private String cor;
+	private String placa;
+	private Tipo tipo;
+	private double valorLocacao;
+	private Status status;
+	
+	
+	public Veiculo(String modelo, String marca, String cor, String placa, String tipo, double valorLocacao) {
+		this.id = Contador.proximoId();
+		
+		this.modelo = modelo;
+		this.marca = marca;
+		this.cor = cor;
+		this.placa = placa;
+		this.tipo = Tipo.valueOf(tipo.toUpperCase());
+		this.valorLocacao = valorLocacao;
+		this.status = Status.LIVRE;		
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static enum Tipo{
+		CARRO,
+		CAMINHAO,
+		MOTO
+	}
+	
+	public static enum Status{
+		LIVRE,
+		ALUGADO
+	}
 }
